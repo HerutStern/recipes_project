7fb722b88bf55a5979206f9dbf5c37c01e8d8e50
HERUT <herut.shtern@gmail.com>
2023-12-20 19:49:40 +0200

    V

---
+++ recipes_project_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecipesProjectAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recipes_project_app'
---
+++ recipes_project_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from recipes_project_app.validators import validator_level
+
+
+# Recipe
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=256, db_column="name", null=False, blank=False),
+    ingredients = models.CharField(max_length=500, db_column='ingredients',
+                                   null=False, blank=False)
+    instructions = models.CharField(max_length=500, db_column='instructions',
+                                   null=False, blank=False)
+    level = models.SmallIntegerField(max_length=10, db_column='level',
+                                     validators=[validator_level])
+
+# Rating
+
+# Create your models here.
